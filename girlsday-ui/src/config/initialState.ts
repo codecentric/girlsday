73de344acbaa4a5e4f7e9011c5068d8df794efe9
Tobias Goeschel <tobias.goeschel@codecentric.de>
2016-04-28 13:53:34 +0200

    Changes that were made during the event

--- girlsday-ui/src/config/initialState.ts
+++ girlsday-ui/src/config/initialState.ts
@@ -11,7 +11,7 @@ export let generateInitialState = () => Map({
   // blocked tasks is a list of tasks that needs another task to unlock it. (MAKE_BREAKFAST => BREAKFAST)
   blockedTasks: List.of(TaskType.BREAKFAST, TaskType.DRIVE_TO_SCHOOL, TaskType.PUT_CLOTHES_ON, TaskType.FEED_BABY),
   // tasks that can be executed by more than one person at the same time
-  parallelTasks: List.of(TaskType.BREAKFAST, TaskType.DRINK_COFFEE, TaskType.PUT_CLOTHES_ON, TaskType.PUT_SHOES_ON, TaskType.PREPARE_FOR_SCHOOL, TaskType.DRIVE_TO_SCHOOL),
+  parallelTasks: List.of(TaskType.BREAKFAST, TaskType.DRINK_COFFEE, TaskType.PUT_CLOTHES_ON, TaskType.PUT_SHOES_ON, TaskType.PREPARE_FOR_SCHOOL, TaskType.DRIVE_TO_SCHOOL, TaskType.CLEAN_KITCHEN, TaskType.GET_DRESSED),
   // the current time
   time: sixOClock,
   // all persons, that are part of the simulation
--- girlsday-ui/src/config/persons.ts
+++ girlsday-ui/src/config/persons.ts
@@ -9,40 +9,41 @@ import * as moment from "moment";
 let mom = () => new Person('👩', List.of(
     new Task(TaskType.BRUSH_TEETH, 3),
     new Task(TaskType.TAKE_A_SHOWER, 15),
-    new Task(TaskType.GET_DRESSED, 15),
     new Task(TaskType.PICK_CLOTHES, 10, TaskType.PUT_CLOTHES_ON),
     new Task(TaskType.MAKE_BREAKFAST, 10, TaskType.BREAKFAST),
+    new Task(TaskType.GET_DRESSED, 15),
     new Task(TaskType.BREAKFAST, 15),
-    new Task(TaskType.DIAPER, 10),
-    new Task(TaskType.PREPARE_FEED_BABY, 15, TaskType.FEED_BABY),
     new Task(TaskType.FEED_BABY, 15),
     new Task(TaskType.DRESS_BABY, 15),
     new Task(TaskType.DRINK_COFFEE, 10),
+    new Task(TaskType.CLEAN_KITCHEN, 5),
     new Task(TaskType.PUT_SHOES_ON, 5),
     new Task(TaskType.DRIVE_TO_SCHOOL, 15)
-), moment().hours(5).minutes(30).toDate());
+), moment().hours(5).minutes(45).toDate());
 
 let dad = () => new Person('👨', List.of(
     new Task(TaskType.BRUSH_TEETH, 3),
     new Task(TaskType.TAKE_A_SHOWER, 15),
     new Task(TaskType.GET_DRESSED, 15),
+    new Task(TaskType.DIAPER, 10),
     new Task(TaskType.BREAKFAST, 15),
     new Task(TaskType.DRINK_COFFEE, 10),
     new Task(TaskType.READ_NEWSPAPER, 5),
-    new Task(TaskType.CLEAN_KITCHEN, 15),
     new Task(TaskType.PUT_SHOES_ON, 5, TaskType.DRIVE_TO_SCHOOL),
     new Task(TaskType.DRIVE_TO_SCHOOL, 15)
-), moment().hours(6).minutes(30).toDate());
+), moment().hours(6).minutes(18).toDate());
 
 let daughter = () => new Person('👧', List.of(
-    new Task(TaskType.BRUSH_TEETH, 3),
     new Task(TaskType.WASH_FACE, 2),
+    new Task(TaskType.BRUSH_TEETH, 3),
+    new Task(TaskType.PREPARE_FEED_BABY, 15, TaskType.FEED_BABY),
     new Task(TaskType.PUT_CLOTHES_ON, 15),
     new Task(TaskType.BREAKFAST, 15),
     new Task(TaskType.PREPARE_FOR_SCHOOL, 10),
+    new Task(TaskType.CLEAN_KITCHEN, 10),
     new Task(TaskType.PUT_SHOES_ON, 10),
     new Task(TaskType.DRIVE_TO_SCHOOL, 15)
-), moment().hours(6).minutes(30).toDate());
+), moment().hours(6).minutes(24).toDate());
 
 let son = () => new Person('👶', List.of(
     new Task(TaskType.BRUSH_TEETH, 3),
@@ -52,6 +53,6 @@ let son = () => new Person('👶', List.of(
     new Task(TaskType.PREPARE_FOR_SCHOOL, 10),
     new Task(TaskType.PUT_SHOES_ON, 10),
     new Task(TaskType.DRIVE_TO_SCHOOL, 15)
-), moment().hours(6).minutes(25).toDate());
+), moment().hours(6).minutes(23).toDate());
 
 export let initialPersonList = () => List.of(mom(), dad(), daughter(), son());
